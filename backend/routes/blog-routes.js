4da8fc2808318fd730bd933d45d5ca59fa193435
Jezrel Dave Sondia <jezreldavesondia@gmail.com>
2022-09-06 11:22:17 +0800

    add get specific blog function

--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { getAllBlogs, addBlog } from '../controller/blog-controller'
+import { getAllBlogs, addBlog, getById } from '../controller/blog-controller'
 
 const blogRouter = express.Router()
 
 blogRouter.get("/", getAllBlogs)
 blogRouter.post("/add", addBlog)
+blogRouter.get("/:id", getById)
 
 export default blogRouter
