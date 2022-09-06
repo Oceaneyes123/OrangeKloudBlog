115cd0d41f02433c8fcd4c9ac1bee8c9709a0f70
Jezrel Dave Sondia <jezreldavesondia@gmail.com>
2022-09-06 10:57:44 +0800

    add blog function added

--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { getAllBlogs } from '../controller/blog-controller'
+import { getAllBlogs, addBlog } from '../controller/blog-controller'
 
 const blogRouter = express.Router()
 
 blogRouter.get("/", getAllBlogs)
+blogRouter.post("/add", addBlog)
 
 export default blogRouter
