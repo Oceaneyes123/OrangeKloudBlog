afaeca515ca35bae1bdf8513b35c1134d00ddf34
Jezrel Dave Sondia <jezreldavesondia@gmail.com>
2022-09-08 11:08:21 +0800

    added get blog by specific user

--- backend/controller/blog-controller.js
+++ backend/controller/blog-controller.js
@@ -45,7 +45,9 @@ export const addBlog = async (req, res, next) => {
     try {
         const session = await mongoose.startSession();
         session.startTransaction();
+        //will save the blog to the databes
         await blog.save({session});
+        //determine the reference user of the blog then save
         existingUser.blogs.push(blog)
         await existingUser.save({session})
         await session.commitTransaction()
@@ -71,4 +73,23 @@ export const getById = async (req, res, next) => {
     }
 
     return res.status(200).json({blog})
+}
+
+
+export const getByUserId = async(req, res, next) => {
+    const userId = req.params.id
+    let userBlogs
+    console.log(userId)
+    try {
+        userBlogs = await User.findById(userId).populate("blogs")
+        console.log(userBlogs)
+    } catch (err) {
+        return console.log(err)
+    }
+
+    if(!userBlogs){
+        return res.status(404).json({message: "No Blogs Found"})
+    }
+
+    return res.status(200).json({blogs: userBlogs})
 }
--- backend/routes/blog-routes.js
+++ backend/routes/blog-routes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getAllBlogs, addBlog, getById } from '../controller/blog-controller'
+import { getAllBlogs, addBlog, getById, getByUserId } from '../controller/blog-controller'
 
 const blogRouter = express.Router()
 
 blogRouter.get("/", getAllBlogs)
 blogRouter.post("/add", addBlog)
 blogRouter.get("/:id", getById)
+blogRouter.get("/user/:id", getByUserId)
 
 export default blogRouter
