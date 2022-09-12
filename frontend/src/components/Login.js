ca128bec331a7bf0fabc2a7a029857a0858f2307
Jezrel Dave Sondia <jezreldavesondia@gmail.com>
2022-09-12 16:44:41 +0800

    connect login backend to frontend

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,10 +1,42 @@
 import { Box, Button, TextField, Typography } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const Login = () => {
+  const [isSignup] = useState(false)
+  const [inputs, setInputs] = useState({
+    name:"",
+    email:"",
+    password:"",
+  })
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState, 
+      [e.target.name] : e.target.value
+    }))
+  }
+
+  const sendRequest = async () => {
+    const res = await axios.post("http://localhost:5000/api/user/login", {
+      email: inputs.email,
+      password: inputs.password
+    }).catch(err =>  console.log(err))
+
+    const data = await res.data
+    return data;
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(inputs)
+    sendRequest()
+  }
+
+
+
   return (
-    <div class="main">
-      <form>
+    <div className="main">
+      <form onSubmit={handleSubmit}>
         <Box 
             display="flex" 
             flexDirection="column" 
@@ -16,11 +48,11 @@ const Login = () => {
             maxWidth={400}
             borderRadius={10}
             >
-          <Typography>Orangekloud Login</Typography>
-          <TextField margin='normal' placeholder='Name'/>
-          <TextField margin='normal' placeholder='Email' type={'email'}/>
-          <TextField margin='normal' placeholder='Password' type={'password'}/>
-          <Button sx={{marginTop: '10px', borderRadius: '10px', background: 'orange'}} margin='normal' variant="contained">Login</Button>
+          <Typography>Orangekloud {isSignup ? 'Sign Up' : 'Login'}</Typography>
+          {isSignup &&  <TextField margin='normal' name="name" placeholder='Name' value={inputs.name} onChange={handleChange}/> }
+          <TextField margin='normal' name="email" placeholder='Email' type={'email'} value={inputs.email} onChange={handleChange}/>
+          <TextField margin='normal' name="password" placeholder='Password' type={'password'}  value={inputs.password} onChange={handleChange}/>
+          <Button sx={{marginTop: '10px', borderRadius: '10px', background: 'orange'}} margin='normal' variant="contained" type="submit">{isSignup ? 'Sign Up' : 'Login'}</Button>
         </Box>
       </form>
     </div>
