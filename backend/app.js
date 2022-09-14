14150218f8ed983e49acc850997737f3c2917267
Jezrel Dave Sondia <jezreldavesondia@gmail.com>
2022-09-14 08:14:55 +0800

    added dynamic port

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ import blogRouter from "./routes/blog-routes"
 import cors from 'cors'
 
 const app = express()
+const PORT = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 app.use("/api/user", router)
@@ -14,5 +15,5 @@ app.use("/api/blog", blogRouter)
 mongoose.connect(
     'mongodb+srv://admin:admin@cluster0.g9irziq.mongodb.net/Blog?retryWrites=true&w=majority'
     )
-    .then(() => app.listen(5000))
+    .then(() => app.listen(PORT))
     .then(() => console.log('Database Connected')).catch((err) => console.log(err))
